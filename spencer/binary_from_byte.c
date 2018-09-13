3f02f9438c2dd0565d1071a86798397809d6b5af
Spencer McDonough <spencer@shmcdono-ubuntu18.04>
2018-09-13 14:32:11 -0700

    write and read bits to byte

--- spencer/binary_from_byte.c
+++ spencer/binary_from_byte.c
@@ -19,11 +19,17 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < ACK_SIZE; i++)
     {
-        uint8_t randNum = (uint8_t)(rand() % 256);
-        memcpy(&ack_packet1.packet_tracker[i], &randNum, 8);
+        // write bits into byte
+        for (int j = 0; j < 8; j++)
+        {
+            ack_packet1.packet_tracker[i] |= (1 << j);
+        }
+
+        // read bits from byte
         printf("ack_packet1.packet_tracket[%d] = %d\n", i, ack_packet1.packet_tracker[i]);
         printf("Bit Sequence for packet_tracker[%d]: ", i);
         for (int j = 0; j < 8; j++)
+            
             printf("%d", (ack_packet1.packet_tracker[i] >> j) & 0x01);
         printf("\n");
     }
