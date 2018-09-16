9f04b914d458df53752a46eac1158b85b4ab54eb
zpazooki <37309176+zpazooki@users.noreply.github.com>
2018-09-15 19:02:37 -0700

    Update client_bool.c

--- spencer/final/client_bool.c
+++ spencer/final/client_bool.c
@@ -14,7 +14,7 @@
 
 #define data_size 1400
 #define UDP_BURST 1
-#define BAD_SERVER_ADDR "10.1.1.2"
+#define BAD_SERVER_ADDR "10.1.1.3"
 #define GOOD_SERVER_ADDR "10.1.2.3"
 
 /*
@@ -105,7 +105,7 @@ int main(int argc, char *argv[])
 	 * STEP 0b: open source files and copy to DRAM
 	*******************************************************************************************************/
   	//opening up a file 
-	pFile = fopen ( filename , "rb" );
+	pFile = fopen ( data.bin , "rb" );
 	f2 = fopen ( "log_client.txt" , "w" ); // log file
 	if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
 
